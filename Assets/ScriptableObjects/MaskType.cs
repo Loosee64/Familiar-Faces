3c9025628f3d72eaf0b64821b4f5df8196b8094b
Padraic-Flaherty <c00317831@setu.ie>
2026-01-31 12:49:53 +0000

    added mask modifiers

--- Assets/ScriptableObjects/MaskType.cs
+++ Assets/ScriptableObjects/MaskType.cs
@@ -1,7 +1,20 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "MaskType", menuName = "Scriptable Objects/MaskType")]
 public class MaskType : ScriptableObject
 {
     public string type = "";
+    // base stats
+    public int defense = 1;
+    public int attack = 1;
+    public int agility = 3;
+    // multipliers
+    public float lightMultiplier = 1.0f;
+    public float heavyMultiplier = 2.0f;
+    public float defenseMultiplier = 1.0f;
+    public float attackMultiplier = 1.0f;
+    public float agilityMultiplier = 1.0f;
+
+
 }
