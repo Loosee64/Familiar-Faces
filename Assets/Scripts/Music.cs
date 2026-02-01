a1fc91517a33eeb7c39b5dc4621774e0b8fb10ed
Josh <joshua.boycehyland15@gmail.com>
2026-02-01 13:24:01 +0000

    added msusiv

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class Music : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+   
+        void Awake()
+        {
+            GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+            if (musicObj.Length > 1)
+            {
+                Destroy(this.gameObject);
+            }
+            DontDestroyOnLoad(this.gameObject);
+        }
+    
 
     // Update is called once per frame
     void Update()
